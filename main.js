b72818dbf87be14cc4f9a145cdbbadd8b7febbaa
dowankim1024 <83197376+dowankim1024@users.noreply.github.com>
2025-01-20 17:57:00 +0900

    sound

--- main.js
+++ main.js
@@ -15,6 +15,13 @@ const GAME_DURATION_SEC = 5;
  const popUpText = document.querySelector('.pop-up__message');
 const popUpRefresh = document.querySelector('.pop-up__refresh');
 
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+
+const bgSound = new Audio('./sound/bg.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const windSound = new Audio('./sound/game_win.mp3');
+
  let started = false;
  let score = 0;
  let timer = undefined;
@@ -40,6 +47,7 @@ popUpRefresh.addEventListener('click',()=>{
     showStopButton();
     showTimerAndScore();
     startGameTimer();
+    playSound(bgSound);
  }
 
  function stopGame(){
@@ -47,12 +55,21 @@ popUpRefresh.addEventListener('click',()=>{
     stopGameTimer();
     hideGameButton();
     showPopUpWithText('REPLAY?');
+    playSound(alertSound);
+    stopSound(bgSound);
  }
 
  function finishGame(win){
     started = false;
     hideGameButton();
-    showPopUpWithText(win? 'YOU WON' : 'YOU LOST')
+    if(win){
+        playSound(windSound);
+    }else{
+        playSound(bugSound);
+    }
+    stopGameTimer();
+    stopSound(bgSound);
+    showPopUpWithText(win? 'YOU WON' : 'YOU LOST');
 }
 
  
@@ -103,6 +120,7 @@ function hidePopUp(){
  }
 
  function initGame() {
+    score = 0;
     field.innerHTML = '';
     gameScore.innerText = CARROT_COUNT;
     //벌레와 당근을 생성한 뒤 field 에 추가
@@ -121,18 +139,26 @@ function onFieldClick(event){
         //당근
         target.remove();
         score++;
+        playSound(carrotSound);
         updateScoreBoard();
         if(score === CARROT_COUNT){
             finishGame(true);
         }
     }else if(target.matches(`.bug`)){
         //벌레
-        stopGameTimer();
+        
         finishGame(false);
     }
 }
 
+function playSound(sound){
+    sound.currentTime = 0;
+    sound.play();
+}
 
+function stopSound(sound){
+    sound.pause();
+}
 
 function updateScoreBoard(){
     gameScore.innerText = CARROT_COUNT-score;
